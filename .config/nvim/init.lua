e6c5a438ece7ed225b372f62721224ef7bd391dd
rudyon <keremberket@gmail.com>
2023-06-23 20:51:09 +0300

    init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,8 @@
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+vim.opt.number = true
+vim.opt.undofile = true
+vim.opt.undolevels = 1000 
+vim.opt.undoreload = 1000
+vim.api.nvim_set_option("clipboard","unnamed")

1864fdd6e8fb4d169774011059068006436ac8a1
rudyon <keremberket@gmail.com>
2023-06-23 20:44:20 +0300

    update

---
+++ backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp -v $HOME/.bashrc .bashrc
+cp -v $HOME/.config/bspwm/bspwmrc .config/bspwm/bspwmrc
+cp -v $HOME/.config/sxhkd/sxhkdrc .config/sxhkd/sxhkdrc
+cp -v $HOME/.config/cava/config .config/cava/config
+cp -v $HOME/.config/kitty/kitty.conf .config/kitty/kitty.conf
+cp -v $HOME/.config/nvim/init.lua .config/nvim/init.lua
+cp -v $HOME/.config/polybar/config.ini .config/polybar/config.ini
